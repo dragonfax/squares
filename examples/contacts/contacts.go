fe982fe64f99b25c07693fba5d3fbc6aaf252ede
Jason Stillwell <dragonfax@gmail.com>
2018-09-10 12:42:41 -0700

    .

--- examples/contacts/contacts.go
+++ examples/contacts/contacts.go
@@ -14,27 +14,23 @@ type ContactCategory struct {
 }
 
 func (cc *ContactCategory) Build(context glt.BuildContext) (glt.Widget, error) {
-	// var themeData glt.ThemeData = glt.Theme.of(context)
 	return &glt.Container{
 		Padding: glt.EdgeInsets{Vertical: 16.0},
 		Decoration: &glt.BoxDecoration{
-			Border: &glt.Border{Bottom: &glt.BorderSide{Color: themeData.dividerColor}},
+			Border: &glt.Border{Bottom: &glt.BorderSide{}},
 		},
-		Child: &glt.DefaultTextStyle{
-			Style: glt.Theme.of(context).textTheme.subhead,
-			Child: &glt.SafeArea{
-				Top:    false,
-				Bottom: false,
-				Child: &glt.Row{
-					CrossAxisAlignment: glt.CrossAxisAlignmentStart,
-					Children: []glt.Widget{
-						&glt.Container{
-							Padding: glt.EdgeInsets{Vertical: 24.0},
-							Width:   72.0,
-							Child:   &glt.Icon{Icon: icon, Color: themeData.primaryColor},
-						},
-						&glt.Expanded{Child: &glt.Column{Children: children}},
+		Child: &glt.SafeArea{
+			Top:    false,
+			Bottom: false,
+			Child: &glt.Row{
+				CrossAxisAlignment: glt.CrossAxisAlignmentStart,
+				Children: []glt.Widget{
+					&glt.Container{
+						Padding: glt.EdgeInsets{Vertical: 24.0},
+						Width:   72.0,
+						Child:   &glt.Icon{Icon: icon, Color: themeData.primaryColor},
 					},
+					&glt.Expanded{Child: &glt.Column{Children: children}},
 				},
 			},
 		},
@@ -47,18 +43,17 @@ var _ glt.StatelessWidget = &ContactItem{}
 
 type ContactItem struct {
 	Icon      glt.IconData
-	lines     []string
+	Lines     []string
 	Tooltip   string
-	onPressed VoidCallback
+	OnPressed VoidCallback
 }
 
 func (ci *ContactItem) Build(context glt.BuildContext) (glt.Widget, error) {
-	// ThemeData themeData = Theme.of(context);
-	var columnChildren []glt.Widget = make([]glt.Widget, 0, len(lines))
-	for _, line := range lines[0:-1] {
+	var columnChildren []glt.Widget = make([]glt.Widget, 0, len(ci.Lines))
+	for _, line := range ci.Lines[0 : len(ci.Lines)-1] {
 		columnChildren = append(columnChildren, &glt.Text{Text: line})
 	}
-	columnChildren = append(columnChildren, &glt.Text{Text: lines[-1], Style: themeData.textTheme.caption})
+	columnChildren = append(columnChildren, &glt.Text{Text: ci.Lines[len(ci.Lines)-1]})
 
 	rowChildren := []glt.Widget{
 		&glt.Expanded{
@@ -69,12 +64,11 @@ func (ci *ContactItem) Build(context glt.BuildContext) (glt.Widget, error) {
 		},
 	}
 
-	if icon != nil {
+	if ci.Icon != nil {
 		rowChildren = append(rowChildren, &glt.SizedBox{
 			Width: 72.0,
 			Child: &glt.IconButton{
-				Icon:      &glt.Icon{Icon: icon},
-				Color:     themeData.primaryColor,
+				Icon:      &glt.Icon{Icon: ci.Icon},
 				OnPressed: onPressed,
 			},
 		})
@@ -87,7 +81,7 @@ func (ci *ContactItem) Build(context glt.BuildContext) (glt.Widget, error) {
 				Children:          rowChildren,
 			},
 		},
-	}
+	}, nil
 }
 
 var _ glt.StatefulWidget = &ContactsDemo{}
@@ -111,7 +105,7 @@ const (
 var _ glt.State = &ContactsDemoState{}
 
 type ContactsDemoState struct {
-	scaffoldKey  glt.GlobalKey
+	scaffoldKey  *glt.GlobalKey
 	appBarHeight float32
 }
 
@@ -124,249 +118,242 @@ func (cds *ContactsDemoState) Init() {
 func (cds *ContactsDemoState) Build(context glt.BuildContext) (glt.Widget, error) {
 	var appBarBehavior AppBarBehavior = AppBarBehaviorPinned
 
-	return &glt.Theme{
-		Data: &glt.ThemeData{
-			Brightness:    glt.BrightnessLight,
-			PrimarySwatch: glt.ColorsIndigo,
-			Platform:      glt.Theme.of(context).platform,
-		},
-		Child: &glt.Scaffold{
-			Key: cds.scaffoldKey,
-			Body: &glt.CustomScrollView{
-				Slivers: []glt.Widget{
-					&glt.SliverAppBar{
-						ExpandedHeight: cds.appBarHeight,
-						Pinned:         appBarBehavior == AppBarBehaviorPinned,
-						Floating:       appBarBehavior == AppBarBehaviorFloating || appBarBehavior == AppBarBehaviorSnapping,
-						Snap:           appBarBehavior == AppBarBehaviorSnapping,
-						Actions: []glt.Widget{
-							&glt.IconButton{
-								Icon:    &glt.Icon{Icon: glt.IconsCreate},
-								Tooltip: "Edit",
-								OnPressed: func() {
-									cds.scaffoldKey.currentState.showSnackBar(&glt.SnackBar{
-										Content: &glt.Text{"Editing isn't supported in this screen."},
-									})
-								},
-							},
-							&glt.PopupMenuButton{
-								OnSelected: func(value AppBarBehavior) {
-									context.setState(func() {
-										appBarBehavior = value
-									})
-								},
-								ItemBuilder: func(context glt.BuildContext) {
-									return []glt.PopupMenuItem{
-										&glt.PopupMenuItem{
-											Value: AppBarBehaviorNormal,
-											Child: &glt.Text{"App bar scrolls away"},
-										},
-										&glt.PopupMenuItem{
-											Value: AppBarBehaviorPinned,
-											Child: &glt.Text{"App bar stays put"},
-										},
-										&glt.PopupMenuItem{
-											Value: AppBarBehaviorFloating,
-											Child: &glt.Text{"App bar floats"},
-										},
-										&glt.PopupMenuItem{
-											Value: AppBarBehaviorSnapping,
-											Child: &glt.Text{"App bar snaps"},
-										},
-									}
-								},
+	return &glt.Scaffold{
+		Key: cds.scaffoldKey,
+		Body: &glt.CustomScrollView{
+			Slivers: []glt.Widget{
+				&glt.SliverAppBar{
+					ExpandedHeight: cds.appBarHeight,
+					Pinned:         appBarBehavior == AppBarBehaviorPinned,
+					Floating:       appBarBehavior == AppBarBehaviorFloating || appBarBehavior == AppBarBehaviorSnapping,
+					Snap:           appBarBehavior == AppBarBehaviorSnapping,
+					Actions: []glt.Widget{
+						&glt.IconButton{
+							Icon:    &glt.Icon{Icon: glt.IconsCreate},
+							Tooltip: "Edit",
+							OnPressed: func() {
+								cds.scaffoldKey.currentState.showSnackBar(&glt.SnackBar{
+									Content: &glt.Text{"Editing isn't supported in this screen."},
+								})
 							},
 						},
-						FlexibleSpace: &glt.FlexibleSpaceBar{
-							Title: &glt.Text{"Ali Connors"},
-							Background: &glt.Stack{
-								Fit: glt.StackFitExpand,
-								Children: []glt.Widget{
-									glt.NewImageFromAsset(
-										glt.Asset{
-											File:    "people/ali_landscape.png",
-											Package: "flutter_gallery_assets",
-										},
-										&glt.Image{
-											Fit:    glt.BoxFitCover,
-											Height: cds.appBarHeight,
-										},
-									),
-									// This gradient ensures that the toolbar icons are distinct
-									// against the background image.
-									&glt.DecoratedBox{
-										Decoration: &glt.BoxDecoration{
-											Gradient: &glt.LinearGradient{
-												Begin:  glt.Alignment(0.0, -1.0),
-												End:    glt.Alignment(0.0, -0.4),
-												Colors: []glt.Color{glt.Color{0x60000000}, glt.Color{0x00000000}},
-											},
+						&glt.PopupMenuButton{
+							OnSelected: func(value AppBarBehavior) {
+								context.setState(func() {
+									appBarBehavior = value
+								})
+							},
+							ItemBuilder: func(context glt.BuildContext) {
+								return []glt.PopupMenuItem{
+									&glt.PopupMenuItem{
+										Value: AppBarBehaviorNormal,
+										Child: &glt.Text{"App bar scrolls away"},
+									},
+									&glt.PopupMenuItem{
+										Value: AppBarBehaviorPinned,
+										Child: &glt.Text{"App bar stays put"},
+									},
+									&glt.PopupMenuItem{
+										Value: AppBarBehaviorFloating,
+										Child: &glt.Text{"App bar floats"},
+									},
+									&glt.PopupMenuItem{
+										Value: AppBarBehaviorSnapping,
+										Child: &glt.Text{"App bar snaps"},
+									},
+								}
+							},
+						},
+					},
+					FlexibleSpace: &glt.FlexibleSpaceBar{
+						Title: &glt.Text{"Ali Connors"},
+						Background: &glt.Stack{
+							Fit: glt.StackFitExpand,
+							Children: []glt.Widget{
+								glt.NewImageFromAsset(
+									glt.Asset{
+										File:    "people/ali_landscape.png",
+										Package: "flutter_gallery_assets",
+									},
+									&glt.Image{
+										Fit:    glt.BoxFitCover,
+										Height: cds.appBarHeight,
+									},
+								),
+								// This gradient ensures that the toolbar icons are distinct
+								// against the background image.
+								&glt.DecoratedBox{
+									Decoration: &glt.BoxDecoration{
+										Gradient: &glt.LinearGradient{
+											Begin:  glt.Alignment(0.0, -1.0),
+											End:    glt.Alignment(0.0, -0.4),
+											Colors: []glt.Color{glt.Color{0x60000000}, glt.Color{0x00000000}},
 										},
 									},
 								},
 							},
 						},
 					},
-					&glt.SliverList{
-						Delegate: &glt.SliverChildListDelegate{[]glt.Widget{
-							&glt.AnnotatedRegion{
-								Value: glt.SystemUiOverlayStyleDark,
-								Child: &glt.ContactCategory{
-									Icon: glt.IconsCall,
-									Children: []glt.Widget{
-										&glt.ContactItem{
-											Icon:    glt.IconsMessage,
-											Tooltip: "Send message",
-											OnPressed: func() {
-												scaffoldKey.currentState.showSnackBar(&glt.SnackBar{
-													Content: &glt.Text{"Pretend that this opened your SMS application."},
-												})
-											},
-											Lines: []string{
-												"(650) 555-1234",
-												"Mobile",
-											},
-										},
-										&ContactItem{
-											Icon:    glt.IconsMessage,
-											Tooltip: "Send message",
-											OnPressed: func() {
-												scaffoldKey.currentState.showSnackBar(&glt.SnackBar{
-													Content: &glt.Text{"A messaging app appears."},
-												})
-											},
-											Lines: []string{
-												"(323) 555-6789",
-												"Work",
-											},
-										},
-										&ContactItem{
-											Icon:    glt.IconsMessage,
-											Tooltip: "Send message",
-											OnPressed: func() {
-												cds.scaffoldKey.currentState.showSnackBar(&glt.SnackBar{
-													Content: &glt.Text{"Imagine if you will, a messaging application."},
-												})
-											},
-											Lines: []string{
-												"(650) 555-6789",
-												"Home",
-											},
-										},
-									},
-								},
-							},
-							&ContactCategory{
-								Icon: glt.IconsContactMail,
+				},
+				&glt.SliverList{
+					Delegate: &glt.SliverChildListDelegate{[]glt.Widget{
+						&glt.AnnotatedRegion{
+							Value: glt.SystemUiOverlayStyleDark,
+							Child: &glt.ContactCategory{
+								Icon: glt.IconsCall,
 								Children: []glt.Widget{
-									&ContactItem{
-										Icon:    glt.IconsEmail,
-										Tooltip: "Send personal e-mail",
+									&glt.ContactItem{
+										Icon:    glt.IconsMessage,
+										Tooltip: "Send message",
 										OnPressed: func() {
-											cds.scaffoldKey.currentState.showSnackBar(&glt.SnackBar{
-												Content: &glt.Text{"Here, your e-mail application would open."},
+											scaffoldKey.currentState.showSnackBar(&glt.SnackBar{
+												Content: &glt.Text{"Pretend that this opened your SMS application."},
 											})
 										},
 										Lines: []string{
-											"ali_connors@example.com",
-											"Personal",
+											"(650) 555-1234",
+											"Mobile",
 										},
 									},
 									&ContactItem{
-										Icon:    glt.IconsEmail,
-										Tooltip: "Send work e-mail",
+										Icon:    glt.IconsMessage,
+										Tooltip: "Send message",
 										OnPressed: func() {
-											cds.scaffoldKey.currentState.showSnackBar(&glt.SnackBar{
-												Content: &glt.Text{"Summon your favorite e-mail application here."},
+											scaffoldKey.currentState.showSnackBar(&glt.SnackBar{
+												Content: &glt.Text{"A messaging app appears."},
 											})
 										},
 										Lines: []string{
-											"aliconnors@example.com",
+											"(323) 555-6789",
 											"Work",
 										},
 									},
-								},
-							},
-							&ContactCategory{
-								Icon: glt.IconsLocationOn,
-								Children: []glt.Widget{
 									&ContactItem{
-										Icon:    glt.IconsMap,
-										Tooltip: "Open map",
+										Icon:    glt.IconsMessage,
+										Tooltip: "Send message",
 										OnPressed: func() {
 											cds.scaffoldKey.currentState.showSnackBar(&glt.SnackBar{
-												Content: &glt.Text{"This would show a map of San Francisco."},
+												Content: &glt.Text{"Imagine if you will, a messaging application."},
 											})
 										},
 										Lines: []string{
-											"2000 Main Street",
-											"San Francisco, CA",
+											"(650) 555-6789",
 											"Home",
 										},
 									},
-									&ContactItem{
-										Icon:    glt.IconsMap,
-										Tooltip: "Open map",
-										OnPressed: func() {
-											cds.scaffoldKey.currentState.showSnackBar(&glt.SnackBar{
-												Content: &glt.Text{"This would show a map of Mountain View."},
-											})
-										},
-										Lines: []string{
-											"1600 Amphitheater Parkway",
-											"Mountain View, CA",
-											"Work",
-										},
+								},
+							},
+						},
+						&ContactCategory{
+							Icon: glt.IconsContactMail,
+							Children: []glt.Widget{
+								&ContactItem{
+									Icon:    glt.IconsEmail,
+									Tooltip: "Send personal e-mail",
+									OnPressed: func() {
+										cds.scaffoldKey.currentState.showSnackBar(&glt.SnackBar{
+											Content: &glt.Text{"Here, your e-mail application would open."},
+										})
 									},
-									&ContactItem{
-										Icon:    glt.IconsMap,
-										Tooltip: "Open map",
-										OnPressed: func() {
-											cds.scaffoldKey.currentState.showSnackBar(&glt.SnackBar{
-												Content: &glt.Text{"This would also show a map, if this was not a demo."},
-											})
-										},
-										Lines: []string{
-											"126 Severyns Ave",
-											"Mountain View, CA",
-											"Jet Travel",
-										},
+									Lines: []string{
+										"ali_connors@example.com",
+										"Personal",
+									},
+								},
+								&ContactItem{
+									Icon:    glt.IconsEmail,
+									Tooltip: "Send work e-mail",
+									OnPressed: func() {
+										cds.scaffoldKey.currentState.showSnackBar(&glt.SnackBar{
+											Content: &glt.Text{"Summon your favorite e-mail application here."},
+										})
+									},
+									Lines: []string{
+										"aliconnors@example.com",
+										"Work",
 									},
 								},
 							},
-							&ContactCategory{
-								Icon: glt.IconsToday,
-								Children: []glt.Widget{
-									&ContactItem{
-										Lines: []string{
-											"Birthday",
-											"January 9th, 1989",
-										},
+						},
+						&ContactCategory{
+							Icon: glt.IconsLocationOn,
+							Children: []glt.Widget{
+								&ContactItem{
+									Icon:    glt.IconsMap,
+									Tooltip: "Open map",
+									OnPressed: func() {
+										cds.scaffoldKey.currentState.showSnackBar(&glt.SnackBar{
+											Content: &glt.Text{"This would show a map of San Francisco."},
+										})
 									},
-									&ContactItem{
-										Lines: []string{
-											"Wedding anniversary",
-											"June 21st, 2014",
-										},
+									Lines: []string{
+										"2000 Main Street",
+										"San Francisco, CA",
+										"Home",
 									},
-									&ContactItem{
-										Lines: []string{
-											"First day in office",
-											"January 20th, 2015",
-										},
+								},
+								&ContactItem{
+									Icon:    glt.IconsMap,
+									Tooltip: "Open map",
+									OnPressed: func() {
+										cds.scaffoldKey.currentState.showSnackBar(&glt.SnackBar{
+											Content: &glt.Text{"This would show a map of Mountain View."},
+										})
 									},
-									&ContactItem{
-										Lines: []string{
-											"Last day in office",
-											"August 9th, 2018",
-										},
+									Lines: []string{
+										"1600 Amphitheater Parkway",
+										"Mountain View, CA",
+										"Work",
+									},
+								},
+								&ContactItem{
+									Icon:    glt.IconsMap,
+									Tooltip: "Open map",
+									OnPressed: func() {
+										cds.scaffoldKey.currentState.showSnackBar(&glt.SnackBar{
+											Content: &glt.Text{"This would also show a map, if this was not a demo."},
+										})
+									},
+									Lines: []string{
+										"126 Severyns Ave",
+										"Mountain View, CA",
+										"Jet Travel",
 									},
 								},
 							},
-						}},
-					},
+						},
+						&ContactCategory{
+							Icon: glt.IconsToday,
+							Children: []glt.Widget{
+								&ContactItem{
+									Lines: []string{
+										"Birthday",
+										"January 9th, 1989",
+									},
+								},
+								&ContactItem{
+									Lines: []string{
+										"Wedding anniversary",
+										"June 21st, 2014",
+									},
+								},
+								&ContactItem{
+									Lines: []string{
+										"First day in office",
+										"January 20th, 2015",
+									},
+								},
+								&ContactItem{
+									Lines: []string{
+										"Last day in office",
+										"August 9th, 2018",
+									},
+								},
+							},
+						},
+					}},
 				},
 			},
 		},
-	}
+	}, nil
 }
