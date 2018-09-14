efb616a368ba7c974005b606d7a8d741bc66631e
Jason Stillwell <dragonfax@gmail.com>
2018-09-13 19:19:44 -0700

    Revert "."
    This reverts commit 78c70038e344835045f170a7276c11f4b351c4f0.

---
+++ examples/contacts/main.go
@@ -0,0 +1,16 @@
+package main
+
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import "github.com/dragonfax/glitter/glt"
+
+func main() {
+
+	glt.RunApp(&glt.MaterialApp{
+		Title: "Contacts App",
+		Color: glt.ColorsGrey,
+		Child: &ContactsDemo{},
+	})
+}
